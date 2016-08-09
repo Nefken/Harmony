4eb0b68f0e4b2111ffed718d49c74bfa002e5c4b
Candeles <candelestf2@gmail.com>
2016-08-09 13:27:44 -0400

    !rtd and server logging

--- harmony.js
+++ harmony.js
@@ -4,35 +4,41 @@
  */
 
 // BGN REQUIRE //
-var Discord = require("./node_modules/discord.js/");
-
+var fs = require("fs");
+var Discord = require("discord.js");
 //var Harmonious = require("./lib/Harmonious.js");
-
 var Auth = require("../harmony-discord-auth/auth.json");
 var Help = require("./help.json");
 // END REQUIRE //
 
+// BGN BOT CREATION & DEBUG LOGGING
+var Bot = new Discord.Client();
+var logStream = fs.createWriteStream("harmony_output.log", {"flags" : "a"});
+// END BOT CREATION & DEBUG LOGGING
+
 // BGN GLOBAL VARS //
-// None
 // END GLOBAL VARS //
 
-// BGN BOT CREATION
-var Bot = new Discord.Client();
-// END BOT CREATION
+// BGN GLOBAL FUNCTIONS //
+var superLog = function (inputstring) {
+	console.log(inputstring);
+	logStream.write(inputstring + "\n");
+};
+// END GLOBAL FUNCTIONS //
 
 // BGN STATE CHANGE EVENT HANDLERS //
 Bot.on("ready", function () {
-	console.log("status: ready");
-	console.log("Connected to:");
+	superLog("status: ready");
+	superLog("Connected to:");;
 	for(i = 0; i < Bot.servers.length; i++){
-		console.log("- " + Bot.servers[i].name);
-		for(j = 0; j > Bot.servers.channels.length; j++){
-			console.log("└──" + Bot.servers.channels[j]);
+		superLog("- " + Bot.servers[i].name);
+		for(j = 0; j < Bot.servers[i].channels.length; j++){
+			superLog("  └──" + Bot.servers[i].channels[j].name);
 		}
 	}
 });
 Bot.on("disconnected", function () {
-	console.log("status: disconnected");
+	superLog("status: disconnected");
 	// > Should I be running a reconnect function here?
 
 	//exit node.js with an error
@@ -42,19 +48,55 @@ Bot.on("disconnected", function () {
 
 // BGN MESSAGE RECEPTION //
 Bot.on("message", function (msg) {
-
 	// BGN MESSAGE PARSING //
+	logStream.write(
+		msg.author.username +
+		" sent in server " +
+		msg.server.name +
+		": \'" +
+		msg.content +
+		"\'");
 	// END MESSAGE PARSING //
 
-	// BGN TESTING COMMANDS //
-	// END TESTING COMMANDS //
+	// Split arguments following "!" by spaces
+	if (msg.content.indexOf("!") === 0) {
+		var args = msg.content.substring(1).split(" ");
 
-	// BGN GENERAL COMMANDS //
+		// BGN TESTING COMMANDS //
+		if (args[0] === "ping") {
+			Bot.reply(msg, "hello!");
+		}
+		// END TESTING COMMANDS //
 
-	// END GENERAL COMMANDS //
+		// BGN GENERAL COMMANDS //
+		// standard rtd command: !rtd (1d20) or !rtd xdy
+		if (args[0] === "rtd") {
+			if (args.length > 1) {
+				var rtdargs = args[1].split("d");
+				var dnum = rtdargs[0];
+				var snum = rtdargs[1];
+				// cap number of dice to 10 because no one needs that power
+				if (dnum > 10) { dnum = 10; }
+				for (i=0; i<dnum; i++) {
+					var dx = Math.floor(Math.random() * (snum - 1) + 1);
+					Bot.sendMessage(
+						msg.channel,
+						msg.author.toString() + " rolled: " + dx
+					);
+				}
+			} else {
+				var d20 = Math.floor(Math.random() * (20 - 1) + 1);
+				Bot.sendMessage(
+					msg.channel,
+					msg.author.toString() + " rolled: " + d20
+				);
+			}
+		}
+		// END GENERAL COMMANDS //
 
-	// BGN MOD TOOLS //
-	// END MOD TOOLS //
+		// BGN MOD TOOLS //
+		// END MOD TOOLS //
+}
 });
 // END MESSAGE RECEPTION //
 
